956b74db8e7e0512a0ad0180bea8dfd96aa356dc
nishikawasasaki <kawaisosu.sosu@gmail.com>
2014-12-21 15:09:42 +0900

    Added tag commands

--- cmdTool.go
+++ cmdTool.go
@@ -29,27 +29,65 @@ func getCommand(args []string) {
 
 }
 
-func setCommand(args []string) {
+func getKeysCommand(args []string) {
 
-	key := args[1]
-	value := args[2]
+	tag := args[1]
 
 	var gc gokuyamaClient.GokuyamaClient
 	gc.Connect(*cmdHostname, *cmdPortNo)
 
-	ret := gc.SetValue(key, value)
-	if ret == true {
-		fmt.Println("registerd")
+	ret, err := gc.GetKeysByTag(tag)
+
+	if err != nil {
+		fmt.Errorf("error: %s\n", err)
 	} else {
-		fmt.Errorf("error: %s\n", ret)
+		fmt.Printf("result: %s\n", ret)
 	}
 
+}
+
+func setCommand(args []string) {
+
+	argsNum := len(flag.Args())
+
+	if argsNum == 3 {
+		// set without tags
+		key := args[1]
+		value := args[2]
+
+		var gc gokuyamaClient.GokuyamaClient
+		gc.Connect(*cmdHostname, *cmdPortNo)
+
+		ret := gc.SetValue(key, value)
+		if ret == true {
+			fmt.Println("registerd")
+		} else {
+			fmt.Errorf("error: %s\n", ret)
+		}
+
+	} else if argsNum == 4 {
+		// set with tags
+		key := args[1]
+		value := args[2]
+		tag := args[3]
+
+		var gc gokuyamaClient.GokuyamaClient
+		gc.Connect(*cmdHostname, *cmdPortNo)
+
+		ret := gc.SetValueWithTag(key, value, tag)
+		if ret == true {
+			fmt.Println("registerd")
+		} else {
+			fmt.Errorf("error: %s\n", ret)
+		}
+	}
 }
 
 func showHelp() {
 	msg := `Usage:
     set -- Set key and value.
-    get -- Get value from the key`
+    get -- Get value from the key
+    tag -- Get keys from the tag`
 	fmt.Println(msg)
 }
 
@@ -66,17 +104,13 @@ func main() {
 	commandName := &flag.Args()[0]
 	// fmt.Println(commandName)
 
-	// if *key == "" && *value == "" {
-	// if *key == "" {
-	// fmt.Println("Option -k is required")
-	// os.Exit(1)
-	// }
-
 	switch {
 	case *commandName == "set":
 		setCommand(flag.Args())
 	case *commandName == "get":
 		getCommand(flag.Args())
+	case *commandName == "tag":
+		getKeysCommand(flag.Args())
 	}
 
 }
